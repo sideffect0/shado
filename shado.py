cb4188e2659c7b0af7d27bbbad334efba1b2474f
sideffect0 <mail3renjith@gmail.com>
2016-09-29 23:33:00 +0530

    update with argument parse

--- shado.py
+++ shado.py
@@ -4,9 +4,12 @@ from __future__ import print_function
 
 import sys
 
-def main(name):
-    print(name[0])
+def main(name, args):
+    if name == 'shado':
+       print("Running shado commands")
+    else:
+       print("Shadowing {}".format(name))
 
 
 if __name__ == '__main__':
-   main(sys.argv)
+   main(sys.argv[0], sys.argv[1:])
